96eb6403c940b68390b4aca98e3bb225d5e4e001
moorland <jahng3@gmail.com>
2015-04-28 18:41:13 -0600

    Part2 Red Green + 2nd Test
    Added 2nd Test

--- Application.Domain.Tests/FinanceTests.cs
+++ Application.Domain.Tests/FinanceTests.cs
@@ -12,7 +12,7 @@ namespace Application.Domain.Tests
     public class FinanceTests
     {
         [Test]
-        public void SharesTest()
+        public void Partitioning_a_list_of_one_item_by_one_produces_a_partition_of_size_one()
         {
             List<Share> sharesList = new List<Share>();
             Share shareOne = new Share();
@@ -24,6 +24,21 @@ namespace Application.Domain.Tests
             Partition partition = partitioner.Partition(sharesList);
             System.Diagnostics.Debug.WriteLine(string.Format("The inside of Size is: {0}", partition.Size.ToString()));
             Assert.AreEqual(1, partition.Size);
+        }
+        [Test]
+        public void Partitioning_a_list_of_two_items_by_one_produces_a_partition_of_size_two()
+        {
+            List<Share> sharesList = new List<Share>();
+            Share shareOne = new Share();
+            shareOne.Maximum = 100;
+            shareOne.Minimum = 13;
+            sharesList.Add(shareOne);
+            sharesList.Add(new Share() { Maximum = 50, Minimum = 10 });
+
+            Partitioner partitioner = new Partitioner(1);
+            Partition partition = partitioner.Partition(sharesList);
+            System.Diagnostics.Debug.WriteLine(string.Format("The inside of Size is: {0}", partition.Size.ToString()));
+            Assert.AreEqual(2, partition.Size);
         }
     }
 }
