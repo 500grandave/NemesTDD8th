50c6a7467a0011da2a9c570a5221ffe884c08916
moorland <jahng3@gmail.com>
2015-04-29 19:06:52 -0600

    Debug inside the While Loop
    System.Diagnostics.Debug.WriteLine(string.Format("Loop {0} at Indent
    Level {1}", total, System.Diagnostics.Debug.IndentLevel));

--- Application.Domain/Partitioner.cs
+++ Application.Domain/Partitioner.cs
@@ -18,19 +18,28 @@ namespace Application.Domain
         public Partition Partition(List<Share> sharesList)
         {
 //            throw new NotImplementedException();
-
-
+            System.Diagnostics.Debug.WriteLine("Starting While Loop");
+            System.Diagnostics.Debug.Indent();
+//
             IList<IList<Share>> partitioningResult = new List<IList<Share>>();
             int total = 0;
             while(total < sharesList.Count)
             {
                 List<Share> subGroup = sharesList.Skip(total).Take(_partitionSize).ToList();
                 partitioningResult.Add(subGroup);
+//
+                if ((total % 2) == 0) 
+                    System.Diagnostics.Debug.Indent();
+                if ((total % 3) == 0)
+                    System.Diagnostics.Debug.Unindent();
+                System.Diagnostics.Debug.WriteLine(string.Format("Loop {0} at Indent Level {1}", total, System.Diagnostics.Debug.IndentLevel));
+//
                 total += _partitionSize;
             }
-
-
-
+//
+            System.Diagnostics.Debug.IndentLevel = 0;
+            System.Diagnostics.Debug.WriteLine("Ending While Loop");
+//
             return new Partition() { PartitioningResult = partitioningResult, Size = sharesList.Count};
         }
     }
